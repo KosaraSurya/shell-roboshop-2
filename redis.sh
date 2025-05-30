36483d8f56b55e140e137db9227d4cd482a4dcd5
Surya Prakash <suryasrinivas765@gmail.com>
2025-05-31 01:31:17 +0530

    roboshop

---
+++ redis.sh
@@ -0,0 +1,20 @@
+#!bin/bash
+
+source ./common.sh
+app_name= redis
+
+CHECK_ROOT
+
+dnf module disable redis -y
+dnf module enable redis:7 -y
+
+dnf install redis -y
+VALIDATE $? "installing redis"
+
+sed -i "s/127.0.0.1/0.0.0.o" -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "change redis conf to allow remote cals"
+
+systemctl enable redis 
+systemctl start redis 
+
+VALIDATE $? "Starting redis"
