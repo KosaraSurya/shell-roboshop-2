a2c22d00497cff33c0508055fbfbd931b5ef7337
Surya Prakash <suryasrinivas765@gmail.com>
2025-05-31 01:39:04 +0530

    roboshop

--- redis.sh
+++ redis.sh
@@ -11,7 +11,7 @@ dnf module enable redis:7 -y
 dnf install redis -y
 VALIDATE $? "installing redis"
 
-sed -i "s/127.0.0.1/0.0.0.0" -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 VALIDATE $? "change redis conf to allow remote cals"
 
 systemctl enable redis 
