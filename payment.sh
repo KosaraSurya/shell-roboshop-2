34be68e6fe6a286291870f88dac304008a3e4ef5
Surya Prakash <suryasrinivas765@gmail.com>
2025-05-31 18:48:26 +0530

    roboshop

--- payment.sh
+++ payment.sh
@@ -11,7 +11,7 @@ app_name
 dnf install python3 gcc python3-devel -y
 VALIDATE $? "Install Python3 packages"
 
-pip3 install -r requirements.txt 
+pip3 install -r requirements.txt
 VALIDATE $? "Installing dependencies"
 
 system_setup
