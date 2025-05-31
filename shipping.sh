671622c16f1e5aa7d52427e7e3e6d78ece27c952
Surya Prakash <suryasrinivas765@gmail.com>
2025-05-31 18:39:39 +0530

    roboshop

--- shipping.sh
+++ shipping.sh
@@ -6,7 +6,7 @@ app_name="shipping"
 
 CHECK_ROOT
 echo -e "$Y enter password"
-read -s mysql_Password
+read -s MYSQL_ROOT_PASSWORD
 
 app_setup
 maven_setup
@@ -15,16 +15,16 @@ system_setup
 dnf install mysql -y 
 VALIDATE $? "installing mysql"
 
-mysql -h mysql.devsecopstrainee.site -u root -p$mysql_Password -e 'use cities'
-if [$? -ne 0]
+mysql -h mysql.devsecopstrainee.site -u root -p$MYSQL_ROOT_PASSWORD -e 'use cities' &>>$LOG_FILE
+if [ $? -ne 0 ]
 then
-    mysql -h mysql.devsecopstrainee.site -uroot -$mysql_Password < /app/db/schema.sql
-    mysql -h mysql.devsecopstrainee.site -uroot -$mysql_Password < /app/db/app-user.sql
-    mysql -h mysql.devsecopstrainee.site -uroot -$mysql_Password < /app/db/master-data.sql
+    mysql -h mysql.devsecopstrainee.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql
+    mysql -h mysql.devsecopstrainee.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql
+    mysql -h mysql.devsecopstrainee.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql
     VALIDATE $? "Loading data into MySQL"
+    
 else
-    echo -e "$Y data is already loaded $N"
+    echo -e "Data is already loaded into MySQL ... $Y SKIPPING $N"
 fi
-
 systemctl restart shipping
-VALIDATE $? "Re-starting shipping"
+VALIDATE $? "Restart shipping"
